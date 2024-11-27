42f4b0699f351099243587d9adc377764768c3db
Eduardo <edunegreiross@gmail.com>
2024-11-27 19:42:16 -0300

    Estilizando a sessão de titulos

---
+++ src/app/components/experience/experience.tsx
@@ -0,0 +1,13 @@
+import { SectionTitle } from "../sectionTitle/section-title";
+
+export function Experience() {
+  return(
+    <div className="experience">
+        <SectionTitle text="Experiência"/>
+        <p>texto</p>
+        <div className="experience-time">
+
+        </div>
+      </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,33 @@
+import { Experience } from "./components/experience/experience";
 import { Header } from "./components/header/header";
 
 import "./styles/home.scss";
 
 export default function Home() {
   return (
-   <main className="container">
-    <Header/>
-    <div className="experience">
-      <h3>experience</h3>
-      <p>texto</p>
-      <div className="experience-time">
+    <main className="container">
+      <Header />
+      <Experience/>
+      
 
-      </div>
-      <div className="infos">
-        <h3>Languages</h3>
-        <div className="languages-info">
-          <span>EN - Intermediary</span>
-          <span>BR - Fluent</span>
-        </div>
-        <h3>Education</h3>
-        <div className="educational-info">
-        <span>🎓</span>
-        <span>Uninove</span>
-        </div>
-      </div>
-      <div className="buttons">
-        <div className="social">
+        <div className="infos">
+          <h3>Languages</h3>
+          <div className="languages-info">
+            <span>EN - Intermediary</span>
+            <span>BR - Fluent</span>
+          </div>
 
+          <h3>Education</h3>
+          <div className="educational-info">
+            <span>🎓</span>
+            <span>Uninove</span>
+          </div>
         </div>
-        <button>Contact me</button>
-      </div>
-    </div>    
-   </main>
+        <div className="buttons">
+          <div className="social"></div>
+          <button>Contact me</button>
+        </div>
+     
+    </main>
   );
 }
