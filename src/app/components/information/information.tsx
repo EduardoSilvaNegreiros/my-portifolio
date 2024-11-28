51b96ac0c152ac843ade3c470a5a3258c8990f2d
Eduardo <edunegreiross@gmail.com>
2024-11-28 17:56:27 -0300

    Atualização projeto

--- src/app/components/information/information.tsx
+++ src/app/components/information/information.tsx
@@ -17,10 +17,20 @@ export function Info() {
         <span>
           Universidade Nove de Julho -{" "}
           <strong>Análise e Desenvolvimento de Sistemas</strong>
+          <br />
+          Início: 02/2024 - Conclusão: 08/2026
         </span>
         <br />
+
         <span>
           CNA Jaraguá - <strong>Curso de Inglês (5 anos)</strong>
+          <br />
+          Início: 03/2020 - Conclusão: 11/2024
+        </span>
+        <br />
+
+        <span>
+          <strong>Curso de JavaScript e TypeScript (Básico ao Avançado)</strong>
         </span>
       </div>
     </div>
