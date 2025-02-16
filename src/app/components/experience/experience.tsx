07aff234a7a407a7d3dbb67f0e6c689316aaaaa2
Eduardo <edunegreiross@gmail.com>
2025-02-16 00:05:39 -0300

    Atualização e melhora do portifólio

--- src/app/components/experience/experience.tsx
+++ src/app/components/experience/experience.tsx
@@ -1,68 +1,39 @@
-import Image from "next/image";
+// components/experience/experience.tsx
 import { SectionTitle } from "../sectionTitle/section-title";
-
+import { ExperienceItem } from "../experience/experienceItem"; // Importando o novo componente
 import "./experience.scss";
 
 export function Experience() {
+  const experienceData = [
+    { logo: "/reactLogo.png", alt: "React logo", time: "1 ano" },
+    { logo: "/node.png", alt: "Node logo", time: "1 ano" },
+    { logo: "/TypescriptLogo.png", alt: "Typescript logo", time: "1 ano" },
+    { logo: "/js.png", alt: "Js logo", time: "1 ano" },
+    { logo: "/csharp.png", alt: "C# logo", time: "1 ano" },
+    { logo: "/html.png", alt: "HTML logo", time: "1 ano" },
+    { logo: "/css.png", alt: "CSS logo", time: "1 ano" },
+    { logo: "/php.png", alt: "PHP logo", time: "1 ano" },
+    { logo: "/laravel.png", alt: "Laravel logo", time: "6 meses" },
+  ];
+
   return (
     <div className="experience">
       <SectionTitle text="Experiência" />
       <p>
         Atualmente, desenvolvo minhas habilidades por meio de hackathons,
         projetos acadêmicos e pessoais, além de cursos online, adquirindo
-        experiência prática e aprofundando meus conhecimentos na área
+        experiência prática e aprofundando meus conhecimentos na área.
       </p>
+
       <div className="experience-time">
-        <div className="experience-language">
-          <Image
-            src="/reactLogo.png"
-            alt="React logo"
-            width={40}
-            height={40}
-            priority
+        {experienceData.map((item, index) => (
+          <ExperienceItem
+            key={index}
+            logo={item.logo}
+            alt={item.alt}
+            time={item.time}
           />
-          <div className="experience-unit">
-            <div className="experience-measure measure-1">1 ano</div>
-          </div>
-        </div>
-        <div className="experience-language">
-          <Image
-            src="/node.png"
-            alt="Node logo"
-            width={40}
-            height={40}
-            priority
-          />
-          <div className="experience-unit">
-            <div className="experience-measure measure-1">1 ano</div>
-          </div>
-        </div>
-        
-        <div className="experience-language">
-          <Image
-            src="/TypescriptLogo.png"
-            alt="Typescript logo"
-            width={40}
-            height={40}
-            priority
-          />
-
-          <div className="experience-unit">
-            <div className="experience-measure measure-1">
-              <span>1 ano</span>
-            </div>
-          </div>
-        </div>
-
-        <div className="experience-language">
-          <Image src="/js.png" alt="Js logo" width={40} height={40} priority />
-          <div className="experience-unit">
-            <div className="experience-measure measure-1">1 ano</div>
-          </div>
-
-
-          
-        </div>
+        ))}
       </div>
     </div>
   );
